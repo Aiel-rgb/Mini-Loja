7a5c66992576bf10c8f3297ba698dee5141af7c6
Aiel-rgb <bielnevesferrer@gmail.com>
2026-01-17 19:32:45 -0300

    Iniciando

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      color:{
+        primary: '#0a1128',
+        secondary: '#001f54',
+        tertiary: '#fefcfb',
+        quaternary: '#1282a2',
+      },
+      
+    },
+  },
+  plugins: [],
+}
